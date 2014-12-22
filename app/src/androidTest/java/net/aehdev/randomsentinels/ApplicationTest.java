c82807fbc887b2e8b42c5fccfd465742c4b13607
Adam Howard <howardad@gmail.com>
2014-12-22 00:01:41 -0500

    Add copyright notices

--- app/src/androidTest/java/net/aehdev/randomsentinels/ApplicationTest.java
+++ app/src/androidTest/java/net/aehdev/randomsentinels/ApplicationTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2014 Adam Howard <aeh AT aehdev DOT net>.
+ *
+ * This file is part of Random Sentinels.
+ *
+ * Random Sentinels is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Random Sentinels is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Random Sentinels. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.aehdev.randomsentinels;
 
 import android.app.Application;
