58a8ac9f5315faa7ffa59d5d8b55a6b2ea2c314a
Adam Howard <howardad@gmail.com>
2015-03-01 22:54:14 -0500

    Parse html files for display on About screen

--- app/src/main/java/net/aehdev/randomsentinels/AboutActivity.java
+++ app/src/main/java/net/aehdev/randomsentinels/AboutActivity.java
@@ -19,15 +19,59 @@
 
 package net.aehdev.randomsentinels;
 
+import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
-public class AboutActivity extends OptionsMenuActivity {
+
+public class AboutActivity extends Activity {
+
+    private StringBuilder oflBangers, calligraphy, butterKnife;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    parseInfo();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
     }
 
+    private void parseInfo() throws IOException {
+        final Resources resources = getResources();
+        InputStream bangersStream = resources.openRawResource(R.raw.ofl_bangers);
+        InputStream calligStream = resources.openRawResource(R.raw.calligraphy);
+        InputStream butterStream = resources.openRawResource(R.raw.butter_knife);
+        BufferedReader bangersReader = new BufferedReader(new InputStreamReader(bangersStream));
+        BufferedReader calligReader = new BufferedReader(new InputStreamReader(calligStream));
+        BufferedReader butterReader = new BufferedReader(new InputStreamReader(butterStream));
+        String line;
+
+        oflBangers = new StringBuilder(bangersStream.available());
+        while ((line = bangersReader.readLine()) != null) {
+            oflBangers.append(line + "\n");
+        }
+
+        calligraphy = new StringBuilder(calligStream.available());
+        while ((line = calligReader.readLine()) != null) {
+            calligraphy.append(line + "\n");
+        }
+
+        butterKnife = new StringBuilder(butterStream.available());
+        while ((line = butterReader.readLine()) != null) {
+            butterKnife.append(line + "\n");
+        }
+    }
 }
